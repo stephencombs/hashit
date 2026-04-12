40caf8c745354efea4c70c94f81298ff0d47c02a
Stephen Combs <sjcombs@paychex.com>
2026-04-12 02:22:00 -0400

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
   plugins: [
+    tailwindcss(),
     tanstackStart(),
     nitro(),
     viteReact(),
